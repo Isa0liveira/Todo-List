44b2ecc0c22e0903e820fa9c4056092e575fd51d
Isabella <isabellaoliveira.trab@gmail.com>
2022-11-24 19:18:13 -0300

    todo-list - v1.6 - mudança na barra de navegação

--- script/todo-list.js
+++ script/todo-list.js
@@ -1,7 +1,7 @@
 const texto = document.querySelector('input')
 const btnInsert = document.querySelector('.divInsert button')
 const btnDeleteAll = document.querySelector('.header button')
-const ul = document.querySelector('ul')
+const ul = document.querySelector('.tarefas')
 
 var itensDB = []
 
